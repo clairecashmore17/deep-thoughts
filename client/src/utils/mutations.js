b8cd8b81d2e549f0204dafe5a1918ef76588df7a
Claire Cashmore <clairecashmore2017@gmail.com>
2022-10-02 14:13:46 -0400

    reaction form added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -39,3 +39,35 @@ export const ADD_FRIEND = gql`
     }
   }
 `;
+
+// mutation for adding a thought
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+//mutation for adding a reaciton
+export const ADD_REACTION = gql`
+  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
